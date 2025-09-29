a7e68b04307f6ae7e16f3965bcaa1a1b3a5ad30e
sebastian lapt <smoreira11@hotmail.com>
2025-09-30 08:08:35 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -3,8 +3,7 @@
 int openingPosition = message.IndexOf('(');
 int closingPosition = message.IndexOf(')');
 
-// Console.WriteLine(openingPosition);
-// Console.WriteLine(closingPosition);
+openingPosition += 1;
 
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));
