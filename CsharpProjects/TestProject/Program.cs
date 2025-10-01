2ad3d4f2fd8c93ead6b134b7d64a6bd0d2310185
sebastian lapt <smoreira11@hotmail.com>
2025-10-01 10:22:01 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,9 +1,6 @@
-﻿string message = "Find what is (inside the parentheses)";
+﻿string message = "hello there!";
 
-int openingPosition = message.IndexOf('(');
-int closingPosition = message.IndexOf(')');
+int first_h = message.IndexOf('h');
+int last_h = message.LastIndexOf('h');
 
-openingPosition += 1;
-
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3fa0d1237a377e3c846c73ad8736c27604e8bfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e68b04307f6ae7e16f3965bcaa1a1b3a5ad30e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
