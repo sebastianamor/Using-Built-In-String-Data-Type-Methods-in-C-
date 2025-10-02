e339fc9931f63005397bd3ec599116d5fac6afed
sebastian lapt <smoreira11@hotmail.com>
2025-10-02 10:34:30 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,45 @@
-﻿string message = "hello there!";
+﻿//string message = "hello there!";
+//int first_h = message.IndexOf('h');
+//int last_h = message.LastIndexOf('h');
+//Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
 
-int first_h = message.IndexOf('h');
-int last_h = message.LastIndexOf('h');
 
-Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
+//string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+//int openingPosition = message.LastIndexOf('(');
+//openingPosition += 1;
+//int closingPosition = message.LastIndexOf(')');
+//int length = closingPosition - openingPosition;
+//Console.WriteLine(message.Substring(openingPosition, length));
+
+// resulatado set of parentheses
+
+
+//string message = "(What if) there are (more than) one (set of parentheses)?";
+//while (true)
+//{
+//    int openingPosition = message.IndexOf('(');
+//    if (openingPosition == -1) break;
+//    openingPosition += 1;
+//    int closingPosition = message.IndexOf(')');
+//    int length = closingPosition - openingPosition;
+//    Console.WriteLine(message.Substring(openingPosition, length));
+
+    // Note the overload of the Substring to return only the remaining 
+    // unprocessed message:
+//    message = message.Substring(closingPosition + 1);
+//}
+
+//resultado What if
+//more than
+//set of parentheses
+
+
+
+string message = "Hello, world!";
+char[] charsToFind = { 'a', 'e', 'i' };
+
+int index = message.IndexOfAny(charsToFind);
+
+Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");
+
+//Found 'e' in 'Hello, world!' at index: 1.
