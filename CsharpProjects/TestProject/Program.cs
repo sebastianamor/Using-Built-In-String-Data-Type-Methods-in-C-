2942ca7e8b3aab8fe4765fc9c6813214bb9cc0c5
sebastian lapt <smoreira11@hotmail.com>
2025-10-04 14:36:08 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -35,11 +35,54 @@
 
 
 
-string message = "Hello, world!";
-char[] charsToFind = { 'a', 'e', 'i' };
+//string message = "Hello, world!";
+//char[] charsToFind = { 'a', 'e', 'i' };
 
-int index = message.IndexOfAny(charsToFind);
+//int index = message.IndexOfAny(charsToFind);
 
-Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");
+//Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");
 
 //Found 'e' in 'Hello, world!' at index: 1.
+
+//string message = "Help (find) the {opening symbols}";
+//Console.WriteLine($"Searching THIS Message: {message}");
+//char[] openSymbols = { '[', '{', '(' };
+//int startPosition = 5;
+//int openingPosition = message.IndexOfAny(openSymbols);
+//Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+
+//openingPosition = message.IndexOfAny(openSymbols, startPosition);
+//Console.WriteLine($"Found WITH using startPosition {startPosition}:  {message.Substring(openingPosition)}");
+
+
+//resulatado =   Found WITHOUT using startPosition: (find) the {opening symbols}
+//resulatado =  Found WITH using startPosition 5:  (find) the {opening symbols}
+
+//string data = "12345John Smith          5000  3  ";
+//string updatedData = data.Remove(5, 20);
+//Console.WriteLine(updatedData);
+
+//resulatado  123455000  3
+
+//string message = "This--is--ex-amp-le--da-ta";
+//message = message.Replace("--", " ");
+//message = message.Replace("-", "");
+//Console.WriteLine(message);
+
+//resultado This is example data
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+//string quantity = "";
+//string output = "";
+
+// Your work here
+
+string quantity = input.Remove(5, 20);
+//message = message.Replace("-", "");
+
+Console.WriteLine(quantity);
+//Console.WriteLine(output);
+
+//Quantity: 5000
+//Output: <h2>Widgets &reg;</h2><span>5000</span>
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad3d4f2fd8c93ead6b134b7d64a6bd0d2310185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e339fc9931f63005397bd3ec599116d5fac6afed")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
